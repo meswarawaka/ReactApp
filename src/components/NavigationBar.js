41e62ffb8841088e7fdc49fb3dc8740ab875d86e
meswarawaka <33041159+meswarawaka@users.noreply.github.com>
2017-12-04 13:12:32 +0530

    Commit file to branch

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -9,12 +9,14 @@ export default () => {
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav>
+				 
                     <NavItem eventKey={1} href="/">Home</NavItem>
                     <NavItem eventKey={2} href="/services">Services</NavItem>
                     <NavItem eventKey={3} href="/contactus">Contact Us</NavItem>
                     <NavItem eventKey={4} href="/signup">Sign Up</NavItem>
                     <NavItem eventKey={5} href="/login">Login</NavItem>
                     <NavItem eventKey={6} href="/feedback">Feedback</NavItem>
+					
                 </Nav>
             </Navbar>
  
